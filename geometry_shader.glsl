f7ea7c411bf0c99b2505a3cc5fb07001429e8518
Vitalya <vitalya.dev@gmail.com>
2026-06-07 00:58:01 +0300

    feat: добавить расчет плоской нормали (flat shading) в геометрический шейдер

--- geometry_shader.glsl
+++ geometry_shader.glsl
@@ -1,36 +1,39 @@
 #version 330
 
-// Сообщаем видеокарте, что на вход ждем треугольники (по 3 вершины)
 layout (triangles) in;
-// На выходе тоже отдаем треугольники
 layout (triangle_strip, max_vertices = 3) out;
 
-// Входные данные от вершинного шейдера (теперь это массив из 3 элементов)
 in vec3 v_geom_position[];
 
-// Выходные данные для фрагментного шейдера
-out vec3 v_position;
+// Теперь мы передаем готовую нормаль вместо позиции
+out vec3 v_normal;
 out vec3 v_barycentric;
 
 void main() {
-    // Обрабатываем первый угол треугольника
+    // Вычисляем два вектора (края нашего треугольника)
+    vec3 edge1 = v_geom_position[1] - v_geom_position[0];
+    vec3 edge2 = v_geom_position[2] - v_geom_position[0];
+    
+    // Векторное произведение дает перпендикуляр (нормаль) ко всей грани
+    vec3 face_normal = normalize(cross(edge1, edge2));
+
+    // Обрабатываем первый угол
     gl_Position = gl_in[0].gl_Position;
-    v_position = v_geom_position[0];
-    v_barycentric = vec3(1.0, 0.0, 0.0); // 100% первый угол
+    v_normal = face_normal;
+    v_barycentric = vec3(1.0, 0.0, 0.0);
     EmitVertex();
 
-    // Обрабатываем второй угол треугольника
+    // Обрабатываем второй угол
     gl_Position = gl_in[1].gl_Position;
-    v_position = v_geom_position[1];
-    v_barycentric = vec3(0.0, 1.0, 0.0); // 100% второй угол
+    v_normal = face_normal;
+    v_barycentric = vec3(0.0, 1.0, 0.0);
     EmitVertex();
 
-    // Обрабатываем третий угол треугольника
+    // Обрабатываем третий угол
     gl_Position = gl_in[2].gl_Position;
-    v_position = v_geom_position[2];
-    v_barycentric = vec3(0.0, 0.0, 1.0); // 100% третий угол
+    v_normal = face_normal;
+    v_barycentric = vec3(0.0, 0.0, 1.0);
     EmitVertex();
 
-    // Завершаем сборку треугольника
     EndPrimitive();
 }
